fbdfc17f8fa76132388343fd1b037740d27ff207
Regis Ximendes <regisximendes.dev@gmail.com>
2023-04-11 22:38:47 -0300

    start commit

---
+++ lib/services/service_locator.dart
@@ -0,0 +1,19 @@
+import 'package:audio_service/audio_service.dart';
+
+import '../page_manager.dart';
+import 'audio_handler.dart';
+import 'playlist_repository.dart';
+import 'package:get_it/get_it.dart';
+
+GetIt getIt = GetIt.instance;
+
+Future<void> setupServiceLocator() async {
+  // services
+  getIt.registerLazySingleton<PlaylistRepository>(() => DemoPlaylist());
+
+  // page state
+  getIt.registerLazySingleton<PageManager>(() => PageManager());
+
+  //start audio service
+  getIt.registerSingleton<AudioHandler>(await initAudioService());
+}
